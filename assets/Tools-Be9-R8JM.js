4450a4e65c5549ec155e0e1ddbd08f5bcae20f0d
Shuyoou <shuyoou@outlook.com>
2025-01-05 22:48:21 +0800

    Updates

--- assets/Tools-Be9-R8JM.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./index-Ck8xaC_Z.js";var o=t("<div>Tools Pages is building~");const a=()=>o();export{a as Tools,a as default};
---
+++ assets/link-Dgefnwi-.js
@@ -0,0 +1,1 @@
+const o="https://shuyoou.gitbook.io/blogs";export{o as B};
